1852a4e1a88ed666f161393b8edbe7f47cc7ce13
Rafael Huaman <leafar.huaman@gmail.com>
2018-11-08 11:56:07 -0500

    fix link

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 shopt -s dotglob
 
+dir="${HOME}/dotfiles/dotfiles/"
+
 for f in ./dotfiles/*; do 
   name=${f##*/}
   echo "Processing ${name} .."; 
-  ln -sfv "$f"  "${HOME}"
+  ln -sfv "${dir}$f"  "${HOME}"
 done
